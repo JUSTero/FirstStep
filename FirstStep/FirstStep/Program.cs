0d927ef8a5363c520a3f40b2cca34f9930d9843a
JUSTero <skorneta@mail.ru>
2022-06-23 16:35:38 +0300

    task resolved

--- FirstStep/FirstStep/Program.cs
+++ FirstStep/FirstStep/Program.cs
@@ -6,11 +6,53 @@ using System.Threading.Tasks;
 
 namespace FirstStep
 {
-    internal class Program
+    class Program
     {
         static void Main()
         {
+            int prev = 0;
 
+            int i = 10;
+
+            int sum = 0;
+
+            while (sum < 26)
+            {
+                Random a = new Random ();
+                int b = a.Next(1001);
+                Console.WriteLine(b);
+
+                if (b > prev)
+                {
+                    int c = b;
+
+                    sum = 0;
+
+                    while (c >= i)
+                    {
+                        sum = sum + c % i;
+
+                        c = c / i;
+                    }
+
+                    sum = sum + c;
+
+                }   
+                    
+                    if (sum < 26)
+                    {
+                        Console.WriteLine(b);
+
+                        prev = b;
+                    }
+                    else
+                    {
+                        Console.WriteLine("STOP");
+
+                        break;
+                    }
+                
+            }
         }
     }
 }
