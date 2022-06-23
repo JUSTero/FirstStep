43d237cc4ad141210d6358f04a97f8eb3165d3d1
JUSTero <skorneta@mail.ru>
2022-06-23 17:38:25 +0300

    task resolve

--- FirstStep/Seminar_2 Task_1/Program.cs
+++ FirstStep/Seminar_2 Task_1/Program.cs
@@ -10,7 +10,43 @@ namespace Seminar_2_Task_3
     {
         static void Main(string[] args)
         {
+            int[] Array = new int[100];
+            int i = 0;
+            string temp;
+            int up = 0;
+            int count = 0;
 
+            do
+            {
+                temp = Console.ReadLine();
+                if (temp == "end")
+                {
+                    break;
+                }
+
+                Array[i] = Convert.ToInt32(temp);
+                i++;
+            }
+            while (true);
+
+            for (i = 0; i < Array.Length - 2; i++)
+            {
+                if (Array[i + 1] > Array[i])
+                {
+                    up += 1;
+                }
+                else
+                {
+                    up = 0;
+                }
+                
+                if (up == 1)
+                {
+                    count += 1;
+                }
+            }
+
+            Console.WriteLine(count);
         }
     }
 }
