8fd1fd497465cadf9b474a06b026698a6f2647b2
JUSTero <skorneta@mail.ru>
2022-06-23 20:07:07 +0300

    rebase folder

---
+++ Seminar_1_CSharp/Seminar_1_CSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar_1_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
